b5d727c34b15112157dfbb831ef81b00c269d4d3
Anurag <anuragtiw112@gmail.com>
2023-03-24 22:04:00 +0530

    Initialized a folder structure for the project

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//import all components here
+import Navbar from "./componentes/Navbar";
+import Home from "./componentes/Home";
+import Sell from "./componentes/Sell";
+import Buy from "./componentes/Buy";
+import Services from "./componentes/Services";
+
+
+function App() {
+  return (
+    <>
+        <Router>
+            <Navbar/>
+            <div>
+                <Routes>
+                    <Route exact path='/' element={<Home/>}/>
+                    <Route exact path='/services/sell' element={<Sell/>}/>
+                    <Route exact path='/services/buy' element={<Buy/>}/>
+                    <Route exact path='/services' element={<Services/>}/>
+                </Routes>
+            </div>
+        </Router>
+    </>
+  );
+}
+
+export default App;
