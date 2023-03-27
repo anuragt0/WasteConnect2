4f3e69a539ff509d19282d6590cbcefcd231aebf
Priyanka verma <priyanka.jat001@gmail.com>
2023-03-27 10:43:23 +0530

    Added home page

--- client/src/componentes/Home.jsx
+++ client/src/componentes/Home.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import "../App.css";
+import RS from "../componentes/images1/RS.png";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className="h">
+    <div className="border">
+
+</div>
+   <div className="left">
+    <h1>WasteConnect India</h1>
+    <p>"Linking farmers and <br/> industries for sustainable <br/> waste management"</p>
+   </div>
+
+   <div className="right">
+      <h2>News/Alerts</h2>
+   </div>
+   <div className="services">
+      <h1>OUR SERVICES</h1>
+    </div>
+</div>
+
+    <div className="page2">
+    
+      <div className="outer-box">
+       <p>Rental Services</p>
+       <div className="line">
+
+       </div>
+      </div>
+      <div className="inner-box">
+      <img src={RS} style={{height:"30rem",width:"38rem"}}></img>
+      </div>
+    </div>
+    </>
   )
 }
 
