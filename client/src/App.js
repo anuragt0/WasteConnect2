d01a43b45549b912ee41ce2c97ef5e46849c79b2
Anurag <anuragtiw112@gmail.com>
2023-03-28 11:43:16 +0530

    Anurag's code

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //import all components here
-import Navbar from "./componentes/Navbar";
-import Home from "./componentes/Home";
+
 import Sell from "./componentes/Sell";
 import Buy from "./componentes/Buy";
-import Services from "./componentes/Services";
 import "./App.css";
 
 function App() {
@@ -15,10 +13,8 @@ function App() {
             {/* <Navbar/> */}
             <div>
                 <Routes>
-                    <Route exact path='/' element={<Home/>}/>
                     <Route exact path='/services/sell' element={<Sell/>}/>
                     <Route exact path='/services/buy' element={<Buy/>}/>
-                    <Route exact path='/services' element={<Services/>}/>
                 </Routes>
             </div>
         </Router>
